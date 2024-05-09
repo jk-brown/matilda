3267044ad017bff78e201d239f03f670e8a156ad
Joe Brown <joseph.brown.k@gmail.com>
2024-05-09 17:28:15 -0400

    Documentation: Editing documentation for historic co2 data to link urls

--- R/data.R
+++ R/data.R
@@ -1,8 +1,14 @@
 #' @title Historic CO2 Concentrations from Climate Indicators 2023
 #'
+#' @description
+#' This is historic CO2 concentration data from the Indicators of Climate Change paper (2023).
+#' It was accessed online via \url{https://zenodo.org/records/8000192} where original code repositories
+#' are linked. We cited the source of the original code repository for this dataset below. This data is used
+#' to create the criterion_historic_co2 weighting criterion.
+#'
 #' @details
 #' Data were accessed on 05/09/2024 from source:
-#' https://github.com/ClimateIndicator/forcing-timeseries/tree/main/data/ghg_concentrations/ar6_updated
+#' \url{https://github.com/ClimateIndicator/forcing-timeseries/tree/main/data/ghg_concentrations/ar6_updated}
 #'
 #' @references Forster, P. M., Smith, C. J., Walsh, T., Lamb, W. F., Lamboll, R., Hauser,
 #' M., Ribes, A., Rosen, D., Gillett, N., Palmer, M. D., Rogelj, J., von Schuckmann, K.,
@@ -13,17 +19,13 @@
 #' Schleussner, C.-F., Szopa, S., Thorne, P., Rohde, R., Rojas Corradi, M., Schumacher, D., Vose,
 #' R., Zickfeld, K., Masson-Delmotte, V., and Zhai, P. : Indicators of Global Climate Change
 #' 2022: annual update of large-scale indicators of the state of the climate system and human influence,
-#' Earth Syst. Sci. Data, 15, 2295–2327, https://doi.org/10.5194/essd-15-2295-2023, 2023.
+#' Earth Syst. Sci. Data, 15, 2295–2327, \url{https://doi.org/10.5194/essd-15-2295-2023}, 2023.
 #'
 #' @format
-#' Prior to pre-processing:
-#' ghg_concentrations.csv: A data frame with 175 observations of 53 variables.
-#' co2_concentrations_update_2019-2023.csv: A data frame with 5 observations of 42 variables
+#'  A data frame with 180 observations of 2 variables
 #'
-#' After pre-processing:
-#' historic_co2_concentrations: A data frame with 180 observations of 2 variables
 #' \describe{
-#' \item{\code{year}}{Integer. Year the data were recorded.}
+#' \item{\code{year}}{Integer. Year the data were recorded from 1750-2023.}
 #' \item{\code{value}}{Numeric. Atmospheric CO2 concentration in parts per million (ppm)}
 #' }
 "historic_co2_concentrations"
